617a05cd751032d06369618002cceefc92e7e603
dhrubaroy <git@trisys.sourcerepo.com:trisys/eduinstitute.git>
2021-09-18 11:51:45 +0200

    first commit

---
+++ HelloWorld/src/com/trisysit/sample/HelloServlet.java
@@ -0,0 +1,51 @@
+package com.trisysit.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/helloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String yourName = request.getParameter("yourName");
+		request.setAttribute("name", yourName);
+		String destination = "response.jsp";
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(destination);		
+		requestDispatcher.forward(request, response);
+//		PrintWriter writer = response.getWriter();
+//		writer.println("Hello " + yourName + ".");
+//		writer.close();
+	}
+
+}
